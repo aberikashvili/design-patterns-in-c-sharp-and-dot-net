eb0de4b82998a1e8aefab52bc6261d3c3eeed466
Arkadi Berikashvili <arkadi.berikashvili90@gmail.com>
2023-05-15 01:42:01 +0400

    Single Responsibility Pattern

--- SolidPrinciples/Program.cs
+++ SolidPrinciples/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+*/
+using SolidPrinciples.Bad;
+using SolidPrinciples.Good;
+
+namespace SolidPrinciples
+{    
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            SingleResponsibilityGood.Demo();
+        }
+    }
+}
