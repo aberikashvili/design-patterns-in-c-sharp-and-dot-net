070600670742bd4de474a8a1c09eb9ba034dd0e2
Arkadi Berikashvili <arkadi.berikashvili90@gmail.com>
2023-05-15 04:08:17 +0400

    code correct

--- SolidPrinciples/OpenClose/OpenCloseGood.cs
+++ SolidPrinciples/OpenClose/OpenCloseGood.cs
@@ -6,109 +6,109 @@ using System.Threading.Tasks;
 
 namespace SolidPrinciples.OpenClose.Good
 {
+    internal enum Color
+    {
+        Red, Green, Blue
+    }
+
+    internal enum Size
+    {
+        Small, Medium, Large
+    }
+
+    internal class Product
+    {
+        public string Name { get; set; }
+
+        public Color Color { get; set; }
+
+        public Size Size { get; set; }
+
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    internal interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    internal interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    internal class ColorSpecification : ISpecification<Product>
+    {
+        private readonly Color _color;
+
+        public ColorSpecification(Color color)
+        {
+            _color = color;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Color == _color;
+        }
+    }
+
+    internal class SizeSpecification : ISpecification<Product>
+    {
+        private readonly Size _size;
+
+        public SizeSpecification(Size size)
+        {
+            _size = size;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Size == _size;
+        }
+    }
+
+    internal class AndSpecification<T> : ISpecification<T>
+    {
+        private readonly ISpecification<T> _first, _second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            _first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            _second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return _first.IsSatisfied(t) && _second.IsSatisfied(t);
+        }
+    }
+
+    internal class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+            {
+                if (spec.IsSatisfied(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+
     internal class OpenCloseGood
     {
-        internal enum Color
-        {
-            Red, Green, Blue
-        }
-
-        internal enum Size
-        {
-            Small, Medium, Large
-        }
-
-        internal class Product
-        {
-            public string Name { get; set; }
-
-            public Color Color { get; set; }
-
-            public Size Size { get; set; }
-
-            public Product(string name, Color color, Size size)
-            {
-                if (name == null)
-                {
-                    throw new ArgumentNullException(paramName: nameof(name));
-                }
-
-                Name = name;
-                Color = color;
-                Size = size;
-            }
-        }
-
-        internal interface ISpecification<T>
-        {
-            bool IsSatisfied(T t);
-        }
-
-        internal interface IFilter<T>
-        {
-            IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
-        }
-
-        internal class ColorSpecification : ISpecification<Product>
-        {
-            private readonly Color _color;
-
-            public ColorSpecification(Color color)
-            {
-                _color = color;
-            }
-
-            public bool IsSatisfied(Product product)
-            {
-                return product.Color == _color;
-            }
-        }
-
-        internal class SizeSpecification : ISpecification<Product>
-        {
-            private readonly Size _size;
-
-            public SizeSpecification(Size size)
-            {
-                _size = size;
-            }
-
-            public bool IsSatisfied(Product product)
-            {
-                return product.Size == _size;
-            }
-        }
-
-        internal class AndSpecification<T> : ISpecification<T>
-        {
-            private readonly ISpecification<T> _first, _second;
-
-            public AndSpecification(ISpecification<T> first, ISpecification<T> second)
-            {
-                _first = first ?? throw new ArgumentNullException(paramName: nameof(first));
-                _second = second ?? throw new ArgumentNullException(paramName: nameof(second));
-            }
-
-            public bool IsSatisfied(T t)
-            {
-                return _first.IsSatisfied(t) && _second.IsSatisfied(t);
-            }
-        }
-
-        internal class BetterFilter : IFilter<Product>
-        {
-            public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
-            {
-                foreach (var i in items)
-                {
-                    if (spec.IsSatisfied(i))
-                    {
-                        yield return i;
-                    }
-                }
-            }
-        }
-
         public static void Demo()
         {
             var apple = new Product("Apple", Color.Red, Size.Small);
